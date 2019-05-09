99aad5a6c3b6504643ab586683d7d8145ffceed4
sas-studio <websidorenko@gmail.com>
2019-05-09 16:04:27 +0300

    index page desktop

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,7 @@
     DOC.addEventListener('DOMContentLoaded', () => {
         const header = DOC.querySelector('#header');
 
-        WIN.onscroll = (e) => {
+        function setHeaderStyle(e) {
             if (pageYOffset === 0) {
                 header.classList.contains('fix') && (header.classList.remove('fix'));
             }
@@ -10,5 +10,8 @@
                 !header.classList.contains('fix') && (header.classList.add('fix'));
             }
         }
+
+        WIN.onscroll = setHeaderStyle;
+        WIN.onload = setHeaderStyle;
     });
 })(document, window);
